1d30fce834f6bb674a21b816d97404685803ab81
Marat Ayupov <maratayup@yandex.ru>
2024-11-11 15:43:38 +0300

    skelet v 0.2

--- UI/UI.java
+++ UI/UI.java
@@ -1,9 +1,19 @@
 package UI;
 
+import java.util.Scanner;
+
 public class UI {
 
     public UI() {
     };
+
+    static public String input() {
+        System.out.println("Введите данные типа double для заполнения вектора");
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+        String phoneNumber = scan.nextLine();
+        return phoneNumber;
+    }
 
     public void run() {
 
--- realization/UserVector.java
+++ realization/UserVector.java
@@ -1,5 +1,27 @@
 package realization;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 public class UserVector {
 
+    private Vector<Boolean> vector;
+
+    public UserVector() {
+
+    };
+
+    public
+
+    static public void splitLine(String line) {
+        String[] arrayLine = line.split(" ");
+        Boolean[] arrayNumbers;
+
+        for (int i = 0; i < arrayLine.length; i++) {
+            arrayNumbers[i] = Boolean.parseBoolean(arrayLine[i]);
+        }
+
+        vector = new Vector<>(Arrays.asList(arrayNumbers));
+
+    }
 }
