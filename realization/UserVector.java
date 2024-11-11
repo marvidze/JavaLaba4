b005282b28d87f27af34d5640bce119d41d39ffd
Vlad Ipatov <royalevlad2@gmail.com>
2024-11-11 15:58:58 +0300

    temp commit

--- realization/UserVector.java
+++ realization/UserVector.java
@@ -1,25 +1,5 @@
 package realization;
 
-import java.util.Arrays;
-import java.util.Vector;
-
 public class UserVector {
 
-    private Vector<Boolean> vector;
-
-    public UserVector(boolean arrayNumbers) {
-        vector = new Vector<>(Arrays.asList(arrayNumbers));
-    };
-
-    static public boolean[] splitLine(String line) {
-        String[] arrayLine = line.split(" ");
-        boolean[] arrayNumbers = new boolean[arrayLine.length];
-
-        for (int i = 0; i < arrayLine.length; i++) {
-            arrayNumbers[i] = Boolean.parseBoolean(arrayLine[i]);
-        }
-
-        return arrayNumbers;
-
-    }
 }
